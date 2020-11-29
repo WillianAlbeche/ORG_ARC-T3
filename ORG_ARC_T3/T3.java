d61adcc523f5dea6451a2c38f91ed6c5cd911c77
Arthur S Dutra <arthurdutra@live.ca>
2020-11-29 18:56:17 -0300

    Update T3.java

--- ORG_ARC_T3/T3.java
+++ ORG_ARC_T3/T3.java
@@ -3,137 +3,215 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class T3 {
 
-	public static String leitor(String path) throws IOException {
-		BufferedReader buffRead = new BufferedReader(new FileReader(path));
-    String linha = "";
-    String frase="";
-		while (true) {
-			if (linha != null) {
-				//System.out.println(linha);
-        frase+=linha;
-			} else
-				break;
-			linha = buffRead.readLine();
-		}
-    buffRead.close();
-    return frase;
-	}
+  private static String path = "Arquivo.txt";
+  private static Scanner in = new Scanner(System.in);
+  private static ArrayList<String> bin = new ArrayList<String>();
+  private static String[] vet;
 
   public static void main(String[] args) throws IOException {
-    Scanner in = new Scanner(System.in);
-    String path="Arquivo.txt";
-  
-    System.out.println("digite endereço: ");
+    startup();
+    // System.out.println("digite endereço: ");
 
-    String entrada="0111111111111100";
+    int opcao;
+    do {
+      System.out.println("-------------------");
+      System.out.println("| 1 - Operacao 1  |");
+      System.out.println("| 2 - Operacao 2  |");
+      System.out.println("| 3 - Operacao 3  |");
+      System.out.println("| 4 - Operacao 4  |");
+      System.out.println("| 0 - Sair        |");
+      System.out.println("-------------------");
 
+      opcao = in.nextInt();
+      in.nextLine();
 
-    String coisa = leitor(path);
-    coisa= coisa.replaceAll(" ", "");
-    String vet[]=coisa.split(",");
-    //  for (int i = 0; i < vet.length; i++) {
-    //    if(i%8 ==0 ){
-    //      System.out.print("\n");
-    //    }
-    //      System.out.print("[ "+vet[i]+" ] ");
-    //  }
+      int mdTag = -1;
+      int mdLinha = -1;
+      String entrada = "RESET";
+      switch (opcao) {
+        case 0:
+          System.out.println("Saindo");
+          break;
+        case 1:
+          // ! Mapeamento direto, com 9 bits para tag, 3 bits para linha
+          mdTag = 9;
+          mdLinha = 3;
+          System.out.println("digite endereço: ");
+          entrada = in.nextLine();
+          while (entrada.length() != 16) {
+            System.out.println("Porfavor digite um endereco com 16 bits");
+            entrada = in.nextLine();
+          }
+          direto(entrada, mdTag, mdLinha);
+          break;
+        case 2:
+          // ! Mapeamento direto, com 9 bits para tag, 4 bits para linha
+          mdTag = 9;
+          mdLinha = 4;
+          System.out.println("digite endereço: ");
+          entrada = in.nextLine();
 
-    ArrayList<String> bin = new ArrayList<String>();
-    for (int i = 0; i < vet.length; i++) {
-        bin.add(converte(vet[i]));
-    }
-    int i=0;
-    // for (String string : bin) {
-      
-    //   System.out.println("["+i+"]-"+string+"-"+ vet[i]);
-    //   i++;
-    // }
-    int j =0;
+          break;
+        case 3:
+          // ! Mapeamento associativo, com 12 bits para tag, 3 bits para palavra
+          break;
+        case 4:
+          // ! Mapeamento associativo, com 13 bits para tag, 2 bits para palavra
+          break;
+        default:
+          System.out.println("Opcao invalida");
+          opcao = in.nextInt();
+          break;
+      }
+    } while (opcao != 0);
+
+    //* exemplo que da hit, String entrada = "0111111111111100";
+  }
+
+  public static void direto(String entrada, int mdTag, int mdLinha) {
+    int j = 0;
     for (String endereco : bin) {
-      String result = mapeamentoDireto(endereco, entrada);
-      System.out.println("["+j+"]-"+endereco+"-"+ result);
+      String result = mapeamentoDireto(endereco, entrada, mdTag, mdLinha);
+      System.out.println("[" + j + "]-" + endereco + "-" + result);
       j++;
     }
-
-
   }
 
-  public static String letra(char c){
-    String letra = "";
-    if(c=='0'){
-      return letra= "0000";
-    }if(c=='1'){
-      return letra= "0001";
-    }if(c=='2'){
-      return letra= "0010";
-    }if(c=='3'){
-      return letra= "0011";
-    }if(c=='4'){
-      return letra= "0100";
-    }if(c=='5'){
-      return letra= "0101";
-    }if(c=='6'){
-      return letra= "0110";
-    }if(c=='7'){
-      return letra= "0111";
-    }if(c=='8'){
-      return letra= "1000";
-    }if(c=='9'){
-      return letra= "1001";
-    }if(c=='a'){
-      return letra= "1010";
-    }if(c=='b'){
-      return letra= "1011";
-    }if(c=='c'){
-      return letra= "1100";
-    }if(c=='d'){
-      return letra= "1101";
-    }if(c=='e'){
-      return letra= "1110";
-    }if(c=='f'){
-      return letra= "1111";
+  public static String mapeamentoDireto(String endereco, String entrada, int mdTag, int mdLinha) {
+    String tagC = "";
+    String lineC = "";
+    String tag = "";
+    String line = "";
+    String result = "";
+    int maxLinha = mdTag + mdLinha + 1;
+
+    for (int i = 0; i < endereco.length(); i++) {
+      if (i < mdTag) {
+        tagC += endereco.charAt(i);
+        tag += entrada.charAt(i);
+      }
+      if (i > mdTag && i < maxLinha) {
+        lineC += endereco.charAt(i);
+        line += entrada.charAt(i);
+      }
     }
-    return letra;
+    if (tagC.equals(tag) && lineC.equals(line)) {
+      result = "HIT";
+    } else
+      result = "MISS";
+
+    return result;
   }
 
+  public static void startup() throws IOException {
+    String coisa = leitor(path);
+    coisa = coisa.replaceAll(" ", "");
+    vet = coisa.split(",");
 
-  public static String converte(String s){
-    String hexa= s;
-    String binario="";
+    for (int i = 0; i < vet.length; i++) {
+      bin.add(converte(vet[i]));
+    }
+  }
+
+  public static void printarBonito(String[] vet) {
+    for (int i = 0; i < vet.length; i++) {
+      if (i % 8 == 0) {
+        System.out.print("\n");
+      }
+      System.out.print("[ " + vet[i] + " ] ");
+    }
+  }
+
+  public static void printarComInfo(String[] vet) {
+    int i = 0;
+    for (String string : bin) {
+      System.out.println("[" + i + "]-" + string + "-" + vet[i]);
+      i++;
+    }
+  }
+
+  public static String leitor(String path) throws IOException {
+    BufferedReader buffRead = new BufferedReader(new FileReader(path));
+    String linha = "";
+    String frase = "";
+    while (true) {
+      if (linha != null) {
+        // System.out.println(linha);
+        frase += linha;
+      } else
+        break;
+      linha = buffRead.readLine();
+    }
+    buffRead.close();
+    return frase;
+  }
+
+  public static String converte(String s) {
+    String hexa = s;
+    String binario = "";
     char bit;
     for (int i = 0; i < hexa.length(); i++) {
-      bit= hexa.charAt(i);
-      binario+=letra(bit) ;
+      bit = hexa.charAt(i);
+      binario += letra(bit);
     }
 
     return binario;
   }
 
-  public static String mapeamentoDireto( String endereco, String entrada){
-    String tagC = "";
-    String lineC = "";
-    String tag = "";
-    String line ="";
-    String result ="";
-      for (int i = 0; i < endereco.length(); i++) {
-          if(i<9){
-            tagC+= endereco.charAt(i);
-            tag+= entrada.charAt(i);
-          }
-          if(i>9 && i<13){
-            lineC+= endereco.charAt(i);
-            line += entrada.charAt(i);
-          }   
-      }
-      if(tagC.equals(tag) && lineC.equals(line)){
-        result ="HIT";
-      }
-      else 
-        result ="MISS";
-
-      return result;
+  public static String letra(char c) {
+    String letra = "";
+    if (c == '0') {
+      return letra = "0000";
+    }
+    if (c == '1') {
+      return letra = "0001";
+    }
+    if (c == '2') {
+      return letra = "0010";
+    }
+    if (c == '3') {
+      return letra = "0011";
+    }
+    if (c == '4') {
+      return letra = "0100";
+    }
+    if (c == '5') {
+      return letra = "0101";
+    }
+    if (c == '6') {
+      return letra = "0110";
+    }
+    if (c == '7') {
+      return letra = "0111";
+    }
+    if (c == '8') {
+      return letra = "1000";
+    }
+    if (c == '9') {
+      return letra = "1001";
+    }
+    if (c == 'a') {
+      return letra = "1010";
+    }
+    if (c == 'b') {
+      return letra = "1011";
+    }
+    if (c == 'c') {
+      return letra = "1100";
+    }
+    if (c == 'd') {
+      return letra = "1101";
+    }
+    if (c == 'e') {
+      return letra = "1110";
+    }
+    if (c == 'f') {
+      return letra = "1111";
+    }
+    return letra;
   }
 
 }
