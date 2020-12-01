6536567ee34c41902e83d02fda181ec0110ae797
Arthur S Dutra <arthurdutra@live.ca>
2020-11-30 23:59:18 -0300

    fixx

--- ORG_ARC_T3/T3.java
+++ ORG_ARC_T3/T3.java
@@ -18,101 +18,99 @@ public class T3 {
     startup();
     int opcao;
 
-    //printarComInfo(bin);
-   //System.out.println("\n\n\n\n");
-
-    System.out.println("------------------------------------------------------");
-    System.out.println("| 1 - Operacao 1- Mapeamento direto linha = 3 bits    |");
-    System.out.println("| 2 - Operacao 2- Mapeamento direto linha = 4 bits    |");
-    System.out.println("| 3 - Operacao 3- Mapemeamento associativo tag = 12   |");
-    System.out.println("| 4 - Operacao 4  Mapemeamento associativo tag = 13   |");
-    System.out.println("| 0 - Sair                                            |");
-    System.out.println("------------------------------------------------------");
-
-    opcao = in.nextInt();
-    in.nextLine();
+    // printarComInfo(bin);
+    // System.out.println("\n\n\n\n");
 
     int mdTag = -1;
     int mdLinha = -1;
+    do {
 
-    switch (opcao) {
-      case 0:
-        System.out.println("Saindo");
-        break;
-      case 1:
-        // ! Mapeamento direto, com 9 bits para tag, 3 bits para linha
-        mdTag = 9;
-        mdLinha = 3;
-        direto(mdTag, mdLinha);
-        break;
-      case 2:
-        // ! Mapeamento direto, com 9 bits para tag, 4 bits para linha
-        mdTag = 9;
-        mdLinha = 4;
-        System.out.println("digite endereço: ");
-        direto(mdTag, mdLinha);
-        break;
-      case 3:
-        // ! Mapeamento associativo, com 12 bits para tag
-        mdTag = 12;
-        System.out.println("digite endereço: ");
-        asociativo(mdTag);
-        break;
-      case 4:
-        // ! Mapeamento associativo, com 13 bits para tag
-        mdTag = 13;
-        asociativo(mdTag);
-        break;
-      default:
-        System.out.println("Opcao invalida");
-        opcao = in.nextInt();
-        break;
-    }
-    while (opcao != 0)
-      ;
+      System.out.println("\n-------------------------------------------------------");
+      System.out.println("| 1 - Operacao 1- Mapeamento direto linha = 3 bits    |");
+      System.out.println("| 2 - Operacao 2- Mapeamento direto linha = 4 bits    |");
+      System.out.println("| 3 - Operacao 3- Mapemeamento associativo tag = 12   |");
+      System.out.println("| 4 - Operacao 4  Mapemeamento associativo tag = 13   |");
+      System.out.println("| 0 - Sair                                            |");
+      System.out.println("-------------------------------------------------------\n");
+
+      opcao = in.nextInt();
+      in.nextLine();
+
+      switch (opcao) {
+        case 0:
+          System.out.println("Saindo");
+          break;
+        case 1:
+          // ! Mapeamento direto, com 9 bits para tag, 3 bits para linha
+          mdTag = 9;
+          mdLinha = 3;
+          direto(mdTag, mdLinha);
+          break;
+        case 2:
+          // ! Mapeamento direto, com 9 bits para tag, 4 bits para linha
+          mdTag = 9;
+          mdLinha = 4;
+          direto(mdTag, mdLinha);
+          break;
+        case 3:
+          // ! Mapeamento associativo, com 12 bits para tag
+          mdTag = 12;
+          asociativo(mdTag);
+          break;
+        case 4:
+          // ! Mapeamento associativo, com 13 bits para tag
+          mdTag = 13;
+          asociativo(mdTag);
+          break;
+        default:
+          System.out.println("\nOpcao invalida");
+          break;
+      }
+
+    } while (opcao != 0);
+
   }
 
-  public static String addicionarAoCache(String data,String tag,String line) {
-    String resultado="Erro";
-    
-    for (cacheLine cacheD: cache) {
+  public static String addicionarAoCache(String data, String tag, String line) {
+    String resultado = "Erro";
+
+    for (cacheLine cacheD : cache) {
       if (cacheD.getTag().equals(tag) && cacheD.getLine().equals(line)) {
         resultado = "HIT";
-      } 
+      }
     }
-    if(cache.size() == 0){
-      cacheLine cacheAdd = new cacheLine(tag,data,line);
+    if (cache.size() == 0) {
+      cacheLine cacheAdd = new cacheLine(tag, data, line);
       cache.add(cacheAdd);
-      resultado ="MISS";
-    }
-     else if(!resultado.equals("HIT")){
-      cacheLine cacheAdd = new cacheLine(tag,data,line);
+      resultado = "MISS";
+    } else if (!resultado.equals("HIT")) {
+      cacheLine cacheAdd = new cacheLine(tag, data, line);
       cache.add(cacheAdd);
       return "MISS";
     }
-    
+
     return resultado;
   }
-  public static String addicionarAoCacheAssociativo(String data,String tag) {
-    String resultado="Erro";
-    
-    for (cacheLine cacheD: cache) {
-      
+
+  public static String addicionarAoCacheAssociativo(String data, String tag) {
+    String resultado = "Erro";
+
+    for (cacheLine cacheD : cache) {
+
       if (cacheD.getTag().equals(tag)) {
         resultado = "HIT";
-      } 
+      }
     }
-    if(cache.size() == 0){
-      cacheLine cacheAdd = new cacheLine(tag,data);
+    if (cache.size() == 0) {
+      cacheLine cacheAdd = new cacheLine(tag, data);
       cache.add(cacheAdd);
-      resultado ="MISS";
-    }
-     else if(!resultado.equals("HIT")){
-      cacheLine cacheAdd = new cacheLine(tag,data);
+      resultado = "MISS";
+    } else if (!resultado.equals("HIT")) {
+      cacheLine cacheAdd = new cacheLine(tag, data);
       cache.add(cacheAdd);
       return "MISS";
     }
-    
+
     return resultado;
   }
 
@@ -127,7 +125,8 @@ public class T3 {
     double missCounter = 0;
     int j = 0;
 
-    System.out.println("\nLinha | Tag       | Palavras        | Resultado ");
+    System.out.println("\n________________________________________________");
+    System.out.println("Linha  | Tag       | Palavras        | Resultado ");
     // System.out.println("000 | 123456789 | 1234567890123456 | 0 ");
     for (String endereco : bin) {
       String tag = endereco.substring(0, 9);
@@ -143,29 +142,28 @@ public class T3 {
         palavras += "XX";
       }
 
-      String result = addicionarAoCache(palavras,tag,linha);
-      
+      String result = addicionarAoCache(palavras, tag, linha);
+
       System.out.println(linha + "   | " + tag + " | " + palavras + " | " + result);
 
-      
       if (result.equalsIgnoreCase("HIT")) {
         hitCounter++;
-        
-      } else if(result.equalsIgnoreCase("MISS")) {
+
+      } else if (result.equalsIgnoreCase("MISS")) {
         missCounter++;
-      }
-      else System.out.println("Deu ruim :(");
+      } else
+        System.out.println("Deu ruim :(");
       j++;
     }
 
     int numTotal = j;
-    System.out.println("counter hit: "+ hitCounter);
-    System.out.println("counter miss: "+ missCounter);
-
-    // System.out.println("\nO endereco " + " tem " + hitCounter + " hits e " +
-    // missCounter + " misses.");
+    System.out.println("\n=============================");
+    System.out.println("counter hit: " + hitCounter);
+    System.out.println("counter miss: " + missCounter);
     System.out.println("Porcentagem de hit:  " + hitFormat.format((hitCounter / numTotal)));
     System.out.println("Porcentagem de miss:  " + missFormat.format((missCounter / numTotal)));
+    System.out.println("=============================\n");
+    
   }
 
   public static void asociativo(int mdTag) {
@@ -176,7 +174,8 @@ public class T3 {
     double missCounter = 0;
     int j = 0;
 
-    System.out.println("\n Tag       | Palavras         | Resultado ");
+    System.out.println("\n___________________________________________");
+    System.out.println("\n Tag          | Palavras         | Resultado ");
     for (String endereco : bin) {
       String tag = "temp";
       String palavras = "temp";
@@ -189,17 +188,17 @@ public class T3 {
         palavras = endereco.substring(0, 13);
         palavras += "XX";
       }
-      String result = addicionarAoCacheAssociativo(palavras,tag);
+      String result = addicionarAoCacheAssociativo(palavras, tag);
       // ! VER ISSO
-      
-        System.out.println( tag + " | " + palavras + " | " + result);
-      
+
+      System.out.println(tag + " | " + palavras + " | " + result);
+
       if (result.equalsIgnoreCase("HIT")) {
         hitCounter++;
       } else {
         missCounter++;
       }
-      if(result.equalsIgnoreCase("ERRO")){
+      if (result.equalsIgnoreCase("ERRO")) {
         System.out.println("ERROOO");
       }
       j++;
@@ -209,12 +208,13 @@ public class T3 {
 
     // System.out.println("\nO endereco " + entrada + " tem " + hitCounter + " hits
     // e " + missCounter + " misses.");
+    System.out.println("\n==================================");
+    System.out.println("counter hit: " + hitCounter);
+    System.out.println("counter miss: " + missCounter);
     System.out.println("Porcentagem de hit:  " + hitFormat.format((hitCounter / numTotal)));
     System.out.println("Porcentagem de miss:  " + missFormat.format((missCounter / numTotal)));
-    System.out.println("counter hit: "+ hitCounter);
-    System.out.println("counter miss: "+ missCounter);
+    System.out.println("==================================\n");
   }
-
 
   public static void startup() throws IOException {
     String coisa = leitor(path);
